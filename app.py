5bd46c225e14fe232cd8b5bf2684bc920252e330
juanfirka14 <juanfirka14@gmail.com>
2026-02-21 13:54:20 +0000

    u

--- app.py
+++ app.py
@@ -662,7 +662,7 @@ modelo_base = SimuladorCentrifuga(config_base)
 f_res_rpm, modos = modelo_base.calcular_frecuencias_naturales()
 # RPM de operación
 
-rpm_range = np.linspace(10, rpm_obj*1.2, 1000)
+rpm_range = np.linspace(10, rpm_obj*1.2, 5000)
 idx_op = np.argmin(np.abs(rpm_range - rpm_obj))
 
 rpm_range, D_desp, D_fuerza, acel_cg, vel_cg, S_desp, S_vel, S_acel = ejecutar_barrido_rpm(modelo_base, rpm_range, d_idx)
